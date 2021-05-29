c4003fdabbdd49e0ba4afae8e312da83c87dcabc
Steven Smith <github@icodeforyou.com>
2021-05-29 03:29:57 -0400

    included addition packages

--- augments/lib/augments.dart
+++ augments/lib/augments.dart
@@ -4,6 +4,7 @@ export 'source/bloc_observer_extension.dart';
 export 'source/context_extensions.dart';
 export 'source/date_time_enums.dart';
 export 'source/date_time_extensions.dart';
+export 'source/int_extensions.dart';
 export 'source/observing_stateful_widget.dart';
 export 'source/widget_extensions.dart';
 export 'source/widget_size.dart';
