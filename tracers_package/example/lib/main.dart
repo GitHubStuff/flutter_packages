309d3aa6f58cde4fe31b3807e974173f60cf2f3f
Steven Smith <github@icodeforyou.com>
2021-05-29 05:37:03 -0400

    updated title

--- tracers_package/example/lib/main.dart
+++ tracers_package/example/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'tracers_package'),
     );
   }
 }
