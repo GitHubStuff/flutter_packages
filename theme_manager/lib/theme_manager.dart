8f0b7afa59502cddb6f4086b43bbf961ea53bd4e
Steven Smith <github@icodeforyou.com>
2021-05-31 18:13:56 -0400

    made cubit local

--- theme_manager/lib/theme_manager.dart
+++ theme_manager/lib/theme_manager.dart
@@ -1,15 +1,8 @@
 library theme_manager;
 
 export 'src/colors/theme_colors.dart';
-// export 'src/app_exceptions.dart';
-// export 'src/colors/theme_colors.dart';
-// export 'src/colors/theme_colors_manager.dart';
 export 'src/cubit/theme_cubit.dart';
 export 'src/theme_manager.dart';
-// export 'src/extensions/text_keys_extension.dart';
-// export 'src/extensions/theme_mode_extensions.dart';
-// export 'src/theme/default_themes.dart';
-// export 'src/theme/text_keys.dart';
 export 'src/widgets/set_theme_dialog.dart';
 export 'src/widgets/theme_control_widget.dart';
 export 'src/widgets/theme_icons.dart';
