c7942677879be309d0d06678f8ce99938be8e203
Steven Smith <github@icodeforyou.com>
2021-06-06 00:15:55 -0400

    removed deprecated code

--- flutter_extras/lib/source/app_exceptions.dart
+++
@@ -1,5 +0,0 @@
-import 'package:app_exception/app_exception.dart';
-
-class CannotReadThemeMode extends AppException {
-  CannotReadThemeMode([String message = '', int code = 903]) : super(message, 'Cannot Read Pref for Theme', code);
-}
