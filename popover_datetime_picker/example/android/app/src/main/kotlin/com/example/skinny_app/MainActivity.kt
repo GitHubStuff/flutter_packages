8f017cd227437c430a15cbb028c65be451bd404f
Steven Smith <github@icodeforyou.com>
2021-07-05 11:06:22 -0400

    moved popover_datetime_picker to own repo

--- popover_datetime_picker/example/android/app/src/main/kotlin/com/example/skinny_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.icodeforyou.popover_datetime_picker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- popover_datetime_picker/lib/popover_datetime_picker.dart
+++
@@ -1,3 +0,0 @@
-library popover_datetime_picker;
-
-export 'source/picker_stack/popover_datetime_picker.dart';
