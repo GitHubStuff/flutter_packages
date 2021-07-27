b2eb58169e5759336f68f5716cfb2818addc795a
Steven Smith <github@icodeforyou.com>
2021-07-27 12:33:11 -0400

    added datetime to identity for logging

--- password_widget/lib/data/identity.dart
+++ password_widget/lib/data/identity.dart
@@ -1,14 +1,19 @@
+import 'package:flutter_extras/source/date_time_extensions.dart';
+
 import '../constants.dart' as K;
 
 class Identity {
   String _email = '';
   String _password = '';
+  String _dateTime = '';
   final K.WidgetType widgetType;
   Identity({required this.widgetType, String email = '', String password = ''}) {
     _email = email.toLowerCase();
     _password = password;
+    _dateTime = DateTimeExtension.asSqlite();
   }
 
   String get email => _email;
   String get password => _password;
+  String get dateTime => _dateTime;
 }
